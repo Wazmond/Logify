272a77f7ddefa6d6b12f6878375751ca68508a2e
Ryuichi <ryuichi.rooochie@gmail.com>
2024-09-22 02:14:35 +1000

    Added clear garage

--- app/(garage)/car/[car].tsx
+++ app/(garage)/car/[car].tsx
@@ -1,16 +1,25 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, TouchableHighlight, View } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { Stack, useLocalSearchParams, useNavigation } from "expo-router";
+import { useDispatch } from "react-redux";
+import { clearGarage } from "@/slices/garageSlice";
+
 
 const CarPage = () => {
   const { car } = useLocalSearchParams();
   const navigation = useNavigation();
+  const dispatch = useDispatch();
 
   useLayoutEffect(() => {
     navigation.setOptions({
       title: car || "default title",
     });
   }, [navigation, car]);
+
+  const handleRemoveVehicle = () => {
+    console.log(clearGarage)
+    dispatch(clearGarage())
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -25,6 +34,11 @@ const CarPage = () => {
           <Text>Vehicle Logs</Text>
         </View>
         <Text style={styles.text}>CarPage, params: {car}</Text>
+      </View>
+      <View>
+        <TouchableHighlight onPress={handleRemoveVehicle}>
+          <Text>REMOVE</Text>
+        </TouchableHighlight>
       </View>
     </SafeAreaView>
   );
