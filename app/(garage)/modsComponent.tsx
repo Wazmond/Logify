a33da3f1edc75330f2a352a55b5eb08d35dd77ff
Ryuichi <ryuichi.rooochie@gmail.com>
2024-10-23 20:40:06 +1100

    Prettier formatting + autofocus text input

--- app/(garage)/modsComponent.tsx
+++ app/(garage)/modsComponent.tsx
@@ -5,7 +5,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { SetStateAction, useEffect, useState } from "react";
+import React, { SetStateAction, useEffect, useRef, useState } from "react";
 import { GarageObject } from "@/slices/garageSlice";
 import { useForm } from "@/constants/hooks";
 
@@ -15,19 +15,27 @@ interface props {
   isOpen: string;
 }
 
-const ModsComponent: React.FC<props> = ({
-  property,
-  setIsOpen,
-  isOpen,
-}) => {
-  const [ inputValue, setInputValue ] = useState("")
-  const { form, setForm } = useForm()
+const ModsComponent: React.FC<props> = ({ property, setIsOpen, isOpen }) => {
+  const [inputValue, setInputValue] = useState("");
+  const { form, setForm } = useForm();
+
+  const inputRef = useRef<TextInput | null>(null);
+
+  useEffect(() => {
+    isOpen === property && setTimeout(() => inputRef.current?.focus(), 1);
+  }, [isOpen, property, form.modifications]);
 
   const handleTextSubmit = () => {
-    inputValue && setForm({...form, modifications: {...form.modifications, 
-    [property]: [...form.modifications[property] || [], inputValue]}})
-    setInputValue("")
-  }
+    inputValue &&
+      setForm({
+        ...form,
+        modifications: {
+          ...form.modifications,
+          [property]: [...(form.modifications[property] || []), inputValue],
+        },
+      });
+    setInputValue("");
+  };
 
   return (
     <View style={styles.component}>
@@ -40,18 +48,22 @@ const ModsComponent: React.FC<props> = ({
           <Text style={styles.title}>{property}</Text>
           {isOpen === property && (
             <View>
-              {form.modifications[property]?.length > 0 &&
-                form.modifications[property].map((value, index) => (
-                  <View key={index}>
-                    <Text>{value}</Text>
-                  </View>
-                ))}
+              <View style={styles.modsComponent}>
+                {form.modifications[property]?.length > 0 &&
+                  form.modifications[property].map((value, index) => (
+                    <View key={index}>
+                      <Text>{value}</Text>
+                    </View>
+                  ))}
+              </View>
               <TextInput
+                ref={inputRef}
                 placeholder="Enter details..."
                 value={inputValue}
                 onChangeText={(e) => {
-                  setInputValue(e)
+                  setInputValue(e);
                 }}
+                style={styles.inputBox}
                 onSubmitEditing={() => handleTextSubmit()}
               />
             </View>
@@ -68,13 +80,26 @@ const styles = StyleSheet.create({
   component: {
     backgroundColor: "#fff",
     marginBottom: 10,
-    borderRadius: 15,
+    borderRadius: 10,
   },
   container: {
-    padding: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
   },
   title: {
     textTransform: "capitalize",
-    fontSize: 18,
+    marginLeft: 5,
+    fontSize: 20,
+  },
+  modsComponent: {
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    gap: 5,
+  },
+  inputBox: {
+    marginTop: 5,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
   },
 });
