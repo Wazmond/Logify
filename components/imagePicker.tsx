4e479d2ee5f223c35161abd9cfb35c8ee82418eb
Ryuichi <ryuichi.rooochie@gmail.com>
2024-10-29 12:46:43 +1100

    Styling change and adding image library functionality

--- components/imagePicker.tsx
+++ components/imagePicker.tsx
@@ -10,12 +10,7 @@ import {
   TouchableWithoutFeedback,
   TouchableOpacity,
 } from "react-native";
-import {
-  launchImageLibrary,
-  launchCamera,
-  ImageLibraryOptions,
-  CameraOptions,
-} from "react-native-image-picker";
+import * as ImagePicker from "expo-image-picker";
 
 interface imagePickerComponentProps {
   imgState: boolean;
@@ -27,11 +22,48 @@ const ImagePickerComponent: React.FC<imagePickerComponentProps> = ({
 }) => {
   const [selectedImage, setSelectedImage] = useState("");
 
-  const launchImagePicker = () => {
+  const libraryPermission = () => {
+    const permission = ImagePicker.getMediaLibraryPermissionsAsync();
   };
 
-  const launchCameraPicker = () => {
+  // const launchImagePicker = () => {
+  //   const options: ImageLibraryOptions = {
+  //     mediaType: "photo",
+  //     includeBase64: false,
+  //     quality: 1,
+  //   };
+
+  //   ImagePicker.launchImageLibraryAsync(options, (response) => {
+  //     if (response.didCancel) {
+  //       console.log("User cancelled image picker");
+  //     } else if (response.errorCode) {
+  //       console.log("Image picker error: ", response.errorMessage);
+  //       Alert.alert("Error", response.errorMessage);
+  //     } else {
+  //       let imageUri = response.assets?.[0]?.uri || "";
+  //       setSelectedImage(imageUri);
+  //     }
+  //   });
+  // };
+
+  const libraryPicker = async () => {
+    let image = await ImagePicker.launchImageLibraryAsync({
+      selectionLimit: 1,
+      aspect: [4, 3],
+      quality: 1,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    });
+    !image.canceled && setSelectedImage(image.assets[0].uri);
+    console.log(selectedImage);
   };
+
+  const launchCameraPicker = async () => {
+    return null
+  };
+
+  const handleImageSave = () => { 
+    setImgState(false)
+   }
 
   return (
     <Modal
@@ -46,14 +78,31 @@ const ImagePickerComponent: React.FC<imagePickerComponentProps> = ({
           <TouchableWithoutFeedback onPress={undefined}>
             <View style={styles.container}>
               <Text style={styles.title}>Add an Image</Text>
+
+              {selectedImage && (
+                <View style={{alignItems: 'center'}}>
+                  <View style={styles.imageView}>
+                    <Image
+                      source={{ uri: selectedImage }}
+                      style={styles.image}
+                    />
+                  </View>
+                  <View>
+                    <TouchableOpacity style={styles.saveButton} onPress={() => handleImageSave()}>
+                      <Text style={styles.saveText}>Save</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
+
               <View style={styles.buttonContainer}>
                 <View style={styles.buttonView}>
-                  <TouchableOpacity onPress={launchImagePicker}>
+                  <TouchableOpacity onPress={libraryPicker}>
                     <Text style={styles.buttonText}>Choose from library</Text>
                   </TouchableOpacity>
                 </View>
                 <View style={styles.buttonView}>
-                  <TouchableOpacity onPress={launchCameraPicker}>
+                  <TouchableOpacity onPress={undefined}>
                     <Text style={styles.buttonText}>Take Photo</Text>
                   </TouchableOpacity>
                 </View>
@@ -63,9 +112,6 @@ const ImagePickerComponent: React.FC<imagePickerComponentProps> = ({
                   </TouchableOpacity>
                 </View>
               </View>
-              {selectedImage && (
-                <Image source={{ uri: selectedImage }} style={styles.image} />
-              )}
             </View>
           </TouchableWithoutFeedback>
         </View>
@@ -91,9 +137,22 @@ const styles = StyleSheet.create({
     width: "80%",
     overflow: "hidden",
   },
+  saveButton: {
+    backgroundColor: "#007AFF",
+    borderRadius: 15,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    marginTop: 20
+  },
+  saveText: { 
+    textAlign: 'center',
+    color: "#fff", 
+    fontSize: 18 
+  },
   buttonContainer: {
     flexDirection: "column",
     width: "100%",
+    marginTop: 20,
   },
   buttonView: {
     borderWidth: 1,
@@ -103,15 +162,19 @@ const styles = StyleSheet.create({
     color: "#007AFF",
     textAlign: "center",
     marginVertical: 10,
+    fontSize: 18
   },
   title: {
     fontSize: 20,
-    marginVertical: 20,
+    marginTop: 20,
     fontWeight: "bold",
   },
+  imageView: {
+    paddingHorizontal: 10,
+  },
   image: {
-    width: 200,
-    height: 200,
+    height: 185,
+    width: 260,
     marginTop: 20,
     borderRadius: 10,
   },
